fc66f9d015b0ef447569c2a80232fedc82eab082
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-05-24 12:08:51 +0900

    0524

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
         loaderFile: './img-loader.ts',
     },
     env: {
-        server_url_: "https://locpham.blog:4000/",
-        server_url: "http://localhost:4000/",
+        server_url: "https://locpham.blog:4000/",
+        server_url_: "http://localhost:4000/",
         ftp_url: "https://locpham.blog/upload/",
     }
 };
